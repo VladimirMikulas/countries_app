c43c4f8256a0290a608b451a561a9624513e6c6a
Vlamik <vladino.mikullas@gmail.com>
2024-06-19 12:40:35 +0200

    Start of Countries Flutter app

---
+++ android/app/src/main/kotlin/com/countries/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.countries.org.countries_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:countries_app/app/app.dart';
+import 'package:countries_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
